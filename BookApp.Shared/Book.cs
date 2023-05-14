4ffdea30130a816d5268e59ae21b942b2f9f19c0
Seolhee <seolhee@campigotto.com>
2023-05-14 03:01:08 -0400

    add models class to match to th Books table

---
+++ BookApp.Shared/Book.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BookApp.Shared
+{
+    public class BookBase
+    {
+        /// <summary>
+        /// Serial Number
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Title of Book
+        /// </summary>
+        [MaxLength(255)]
+        [Required(ErrorMessage ="Enter the title of book")]
+        [Column(TypeName = "NVarChar(255)")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Description of the Book
+        /// </summary>
+        public string Description { get; set; }
+    }
+    [Table("Books")]
+    public class Book : BookBase
+    {
+        //Empty
+    }
+}
